f40930751479b8082996c349ab93b8351687ce60
Malevych Yurii <yuriimalevych@gmail.com>
2024-10-21 21:35:06 +0300

    some practice in arrays (start array methods)

---
+++ Arrays/2PracticeArrays.js
@@ -0,0 +1,22 @@
+'use strict'
+
+const arr = [15, 2, 4, 29, 93];
+// let words = ["Hello", "world", "how", "are", "you"];
+// let sentence = words.join(" ");
+// console.log(sentence); перетворює array на string, об'єднує всі елементи масиву в 1 рядок
+
+// let str = "Hello";
+// let strArr = str.split(""); // перетворює string в array і розбиває рядок на окремі символи
+
+// arr.push(5); // додає елемент в кінець масиву s
+// arr.pop(); // видаляє останній елемент з масиву і повертає його
+// arr.shift(); // видаляє перший елемент з масиву і порвертає його
+//arr.unshift('q'); // додає елемент в масив на початок
+// delete arr[2]; // видаляє елемент масиву по індексу
+
+// for (let i in arr) {
+//     console.log(`Text ${i}`);
+// } // перебір індексів
+
+console.log(arr);
+// console.log(strArr);

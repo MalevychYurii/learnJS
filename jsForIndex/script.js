184ac7001ea370870b9104dbe4ee7efcaf858bb3
Malevych Yurii <yuriimalevych@gmail.com>
2024-11-08 15:45:24 +0200

    DOM practice №2

--- jsForIndex/script.js
+++ jsForIndex/script.js
@@ -1,15 +1,39 @@
-'use strict'
+const box = document.getElementById('box'),
+    btns = document.getElementsByTagName('button'),
+    circles = document.getElementsByClassName('circle'),
+    wrapper = document.querySelector('.wrapper'),
+    hearts = wrapper.querySelectorAll('.heart'),
+    oneHeart = wrapper.querySelector('.heart');
 
-const box = document.getElementById('box');
+console.log(btns);
 
-console.log(box);
+box.style.backgroundColor = 'blue';
+box.style.width = '500px';
 
-const btns = document.getElementsByTagName('button');
+btns[1].style.borderRadius = '100%';
+circles[0].style.backgroundColor = 'red';
 
-console.log(...btns);
-
-const hearts = document.querySelectorAll('.heart');
+// for (let i = 0; i < hearts.length; i++) {
+//     hearts[i].style.backgroundColor = 'blue';
+// }
 
 hearts.forEach(item => {
-    console.log(item);
+    item.style.backgroundColor = 'green';
 });
+
+const div = document.createElement('div');
+
+div.classList.add('black');
+document.body.append(div);
+div.innerHTML = "<h1>Hi!</h1>";
+
+// circles[0].before(div);
+// circles[0].after(div);
+// hearts[0].replaceWith(circles[0]);
+// hearts[1].after(div);
+
+const test = document.createElement('div');
+
+test.classList.add('black');
+document.body.prepend(test);
+test.innerHTML = "<h1>Hi</h1>";

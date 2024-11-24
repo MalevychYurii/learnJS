61522ffb4ef563f33158c29712ea3d25c82a776c
Malevych Yurii <yuriimalevych@gmail.com>
2024-11-24 13:07:22 +0200

    complete all task from 48 Lesson

--- js (From Lessons)/project(DOM)/js/practice48Lesson.js
+++ js (From Lessons)/project(DOM)/js/practice48Lesson.js
@@ -1,56 +1,62 @@
 'use strict';
 
-const movieDB = {
-    movies: [
-        "Логан",
-        "Лига справедливости",
-        "Ла-ла лэнд",
-        "Одержимость",
-        "Скотт Пилигрим против..."
-    ]
-};
+document.addEventListener('DOMContentLoaded', () => {
 
-const deleteAdd = document.querySelectorAll('.promo__adv img'),
-    newBackground = document.querySelector('.promo__bg'),
-    newGenre = newBackground.querySelector('.promo__genre'),
-    movieList = document.querySelector('.promo__interactive-list'),
-    inputFilm = document.querySelector('.adding__input'),
-    checkboxVerify = document.querySelector('.checkbox'),
-    buttomAddFilfFromInput = document.querySelector('.button');
+    const movieDB = {
+        movies: [
+            "Логан",
+            "Лига справедливости",
+            "Ла-ла лэнд",
+            "Одержимость",
+            "Скотт Пилигрим против..."
+        ]
+    };
 
-deleteAdd.forEach((img) => {
-    img.remove();
-}); // 1 Lesson 44
+    const deleteAdd = document.querySelectorAll('.promo__adv img'),
+        newBackground = document.querySelector('.promo__bg'),
+        newGenre = newBackground.querySelector('.promo__genre'),
+        movieList = document.querySelector('.promo__interactive-list'),
+        inputFilm = document.querySelector('.adding__input'),
+        checkboxVerify = document.querySelector('.checkbox'),
+        buttomAddFilfFromInput = document.querySelector('.button');
 
-newGenre.textContent = "Drama"; // 2 Lesson 44
+    deleteAdd.forEach((img) => {
+        img.remove();
+    }); // 1 Lesson 44
 
-newBackground.style.backgroundImage = "url('img/bg.jpg')"; // 3 Lesson 44
+    newGenre.textContent = "Drama"; // 2 Lesson 44
 
-movieDB.movies.sort();
-movieList.innerHTML = "";
-movieDB.movies.forEach((film, i) => {
-    movieList.innerHTML += `
-        <li class="promo__interactive-item">${i + 1}. ${film}
-            <div class="delete"></div>
-        </li>
-    `;
-}); // 4 + 5 Lesson 44
+    newBackground.style.backgroundImage = "url('img/bg.jpg')"; // 3 Lesson 44
 
-buttomAddFilfFromInput.addEventListener('click', clickFunction);
+    sortFunction();
+    movieList.innerHTML = "";
+    createMovieList();// 4 + 5 Lesson 44
 
-function clickFunction(newFilm) {
-    newFilm = inputFilm.value;
-    if (newFilm != '') {
-        movieDB.movies.push(newFilm);
-        movieDB.movies.sort((a, b) => a.toLowerCase().localeCompare(b.toLowerCase())); // 5
-        movieList.innerHTML = '';
-        if (checkboxVerify.checked) {
-            console.log('Добавлено улюблений фільм'); // 4
+    buttomAddFilfFromInput.addEventListener('click', clickFunction);
+
+    function clickFunction(newFilm) { // 1
+        newFilm = inputFilm.value.trim(); // trim() - забирає лишні пробіли
+        if (newFilm !== '') {
+            movieDB.movies.push(newFilm);
+            sortFunction();
+            movieList.innerHTML = '';
+            if (checkboxVerify.checked) {
+                console.log('Добавлено улюблений фільм'); // 4
+            }
+            console.log(movieDB.movies);
+            createMovieList();
+            inputFilm.value = '';
         }
-        console.log(movieDB.movies);
+    }; // 1+2 Lesson 48
+
+
+    function sortFunction() {
+        movieDB.movies.sort((a, b) => a.toLowerCase().localeCompare(b.toLowerCase())); // 5
+    }
+    function createMovieList() {
         movieDB.movies.forEach((newFilm, i) => {
             if (newFilm.length >= 21) {
-                newFilm = newFilm.slice(0, 21) + '...';
+                newFilm = `${newFilm.slice(0, 21)}...`; // 2
             }
             movieList.innerHTML += `
                 <li class="promo__interactive-item">${i + 1}. ${newFilm}
@@ -58,5 +64,14 @@ function clickFunction(newFilm) {
                 </li>
                 `;
         });
+
+        document.querySelectorAll('.delete').forEach((btn, i) => { // 3
+            btn.addEventListener('click', () => {
+                btn.parentElement.remove();
+                movieDB.movies.splice(i, 1);
+                movieList.innerHTML = '';
+                createMovieList();
+            })
+        });
     }
-}; // 1+2 Lesson 48
+});

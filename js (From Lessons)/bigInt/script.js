35d1365cf1635f05b25873dee74e32059fd7ced3
Malevych Yurii <yuriimalevych@gmail.com>
2025-01-13 15:30:01 +0200

    bitInt + set

---
+++ js (From Lessons)/bigInt/script.js
@@ -0,0 +1,9 @@
+'use strict';
+
+const bigInt = 2141242352352352352352523523523523523523n;
+
+const sameBigInt = BigInt(2141242352352352352352523523523523523523);
+
+console.log(bigInt ** 2)
+
+// console.log(5n + 5) // error

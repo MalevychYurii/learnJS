1d897723213dc7a3b3d2598d7f12502bb72d60ff
Malevych Yurii <yuriimalevych@gmail.com>
2025-01-16 18:15:42 +0200

    setTimeout + setInterval

---
+++ js (From Lessons)/setTimeout + setInterval/script.js
@@ -0,0 +1,36 @@
+const btn = document.querySelector('.btn');
+let timerId,
+    i = 0;
+
+function myAnimation() {
+    const element = document.querySelector('.box');
+    let position = 0;
+
+
+    const id = setInterval(frame, 1);
+    function frame() {
+        if (position == 300) {
+            clearInterval(id);
+        } else {
+            position++;
+            element.style.top = position + 'px';
+            element.style.left = position + 'px';
+        }
+    }
+}
+
+btn.addEventListener('click', myAnimation);
+
+// function logger() {
+//     if (i === 3) {
+//         clearInterval(timerId);
+//     }
+//     console.log('text');
+//     i++
+// }
+
+// let id = setTimeout(function log() {
+//     console.log('Hello');
+
+//     id = setTimeout(log, 500)
+// }, 500)

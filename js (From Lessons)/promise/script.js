28ad960bb4136961941c016fe4b9e604fd0e3449
Malevych Yurii <yuriimalevych@gmail.com>
2025-02-08 13:32:00 +0200

    fetch

--- js (From Lessons)/promise/script.js
+++ js (From Lessons)/promise/script.js
@@ -15,9 +15,37 @@ const req = new Promise(function (resolve, reject) {
     }, 2000);
 });
 
-req.then(() => {
-    setTimeout((product) => {
-        product.status = 'order',
-            console.log(product);
-    })
-})
+req.then((product) => {
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+            product.status = 'order',
+            resolve(product);
+        }, 2000);
+    });
+}).then(data => {
+    data.modify = true;
+    return data;
+}).then(data => {
+    console.log(data)
+}).catch(() => {
+    console.error('Error')
+}).finally(() => {
+    console.log('finally')
+});
+
+const test = time => {
+    return new Promise((resolve, reject) => {
+        setTimeout(() => resolve(), time);
+    });
+};
+
+test(1000).then(() => console.log('1000ms'));
+test(2000).then(() => console.log('2000ms'));
+
+Promise.all([test(1000), test(2000)]).then(() => {
+    console.log('All promises completed!');
+});
+
+Promise.race([test(1000), test(2000)]).then(() => {
+    console.log('Last promise completed');
+});
--- js (From Lessons)/promise/script2.js
+++ js (From Lessons)/promise/script2.js
@@ -9,4 +9,56 @@ promise.then(() => {
     setTimeout(() => {
         console.log(`Second log`)
     }, 2000);
+}); // 1
+
+const promise2 = new Promise((resolve, reject) => {
+    setTimeout(() => {
+        // resolve();
+        reject();
+    })
 });
+
+promise2.then(() => {
+    console.log(`Completed`)
+}).catch(() => {
+    console.log(`Failed`)
+}); // 2
+
+function sendForm(data) {
+    return new Promise((resolve,reject) => {
+        setTimeout(() => {
+            if (data.name) {
+                resolve(`Completed`);
+            } else {
+                reject(`Failed`);
+            }
+        }, data.name ? 2000 : 1000);
+    });
+}
+
+sendForm({ name: "Роман", email: "roman@example.com" })
+    .then(response => console.log(response))
+    .catch(error => console.error(error)); // 3
+
+
+function task1() {
+    return new Promise((resolve, reject) => {
+        resolve(`log`);
+    })
+}
+
+function task2() {
+    return new Promise((resolve, reject) => {
+        resolve(`log`);
+    })
+}
+
+function task3() {
+    return new Promise((resolve, reject) => {
+        resolve(`log`);
+    })
+}
+
+Promise.all([task1, task2, task3]).then(() => {
+    console.log(`Completed`)
+}) // 4
